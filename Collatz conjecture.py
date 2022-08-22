0151a9cd2eeaebed474e25a52d5940f34b00a3a2
Roni <90357658+roniraijin@users.noreply.github.com>
2022-08-23 02:45:50 +0300

    Update Collatz conjecture.py

--- Collatz conjecture.py
+++ Collatz conjecture.py
@@ -1,17 +1,15 @@
-#I was watching a video by Veritasium about the Collatz Conjecture and thought it would be fun to make my own program for it! 
+#I was watching a video by Veritasium about the Collatz Conjecture and this my super basic attempt at making one!
 #If you come across this and see I've made a mistake please contact me :D
 end = False
 number = int(input("Give me a number \n"))
 counter = 0
 while not end:
-
-    if number % 2 == 0:
-        number = number / 2
-
     if number == 1:
         end = True
-
-    if not number % 2 == 0:
+    if number % 2 == 0:
+        number = number / 2
+        
+    elif not number % 2 == 0:
         number *= 3
         number += 1
     counter += 1

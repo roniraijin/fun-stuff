63dff1ba20aa0cd1e88f05acd52a5495ee337f56
Roni <90357658+roniraijin@users.noreply.github.com>
2024-09-05 04:32:13 +0300

    Update Collatz conjecture.py
    minor changes to give more info

--- Collatz conjecture.py
+++ Collatz conjecture.py
@@ -1,17 +1,17 @@
-#I was watching a video by Veritasium about the Collatz Conjecture and this my super basic attempt at making one!
-#If you come across this and see I've made a mistake please contact me :D
 end = False
 number = int(input("Give me a number \n"))
+original_number = number
 counter = 0
 while not end:
+    print(number)
     if number == 1:
         end = True
-    if number % 2 == 0:
+    elif number % 2 == 0:
         number = number / 2
-        
+
     elif not number % 2 == 0:
         number *= 3
         number += 1
     counter += 1
 
-print(counter)
+print(f"It took {original_number}, {counter} steps to reach 1!")
